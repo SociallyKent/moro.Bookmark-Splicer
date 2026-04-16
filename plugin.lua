c203ea15a3031e51f5dc9f1dbf37fb74d4a23936
SociallyKent <137476256+SociallyKent@users.noreply.github.com>
2026-04-16 12:25:55 -0500

    Minor improvements

--- plugin.lua
+++ plugin.lua
@@ -1,12 +1,10 @@
 game =
 	{
 	factors = {},
-	get = {},
 	groups = {},
 	marks = {},
 	notes = {},
 	points = {},
-	sort = {},
 	velocities == {},
 	}
 kit = {}
@@ -27,6 +25,25 @@ DataSheet =
 	RAW = function(v)
 		return v end,
 	}
+_Size =
+	{
+	Infinite = vector.new(-1, -1),
+	Regular = vector.new(0, 0),
+	Micro = vector.new(1, 1),
+	Button = vector.new(80, 20),
+	}
+_Bookmarks =
+	{
+	Active = false,
+	Notes = {},
+	Start = 0,
+	End = 0,
+	InText = "",
+	Marks = {},
+	Quarry = {},
+	Remove = {},
+	}
+Infinite = 1/0
 function StartUp()
 	u = 
 		{
@@ -41,10 +58,7 @@ function StartUp()
 	
 	iButton = imgui.Button
 	iTextFormatless = imgui.TextUnformatted
-	uKeyPressed = utils.IsKeyPressed
 	StartUp = function()
-		MouseDownRight = imgui.IsMouseDown(1)
-		-- imgui.SetNextWindowSizeConstraints({170, 48}, {170, math.huge})
 		iPushVar(ivar.WindowBorderSize, 5)
 	end end
 function string.split(STRING, FORMAT)
@@ -61,12 +75,28 @@ function game.SetupSelection(START, END)
 		return START, END end
 	local Notes = state.SelectedHitObjects
 	return game.SetupOffsets(Notes) end
-function game.SetupOffsets(NOTES)
-	local NOTES = NOTES or state.SelectedHitObjects
-	if #NOTES == 0 then
+function game.SetupOffsets(TABLE)
+	local TABLE = TABLE or state.SelectedHitObjects
+	if #TABLE == 0 then
 		return -1, -1 end
-	local Start, End = NOTES[1].StartTime, NOTES[#NOTES].StartTime
+	local Start, End = TABLE[1].StartTime, TABLE[#TABLE].StartTime
 	return Start, End end
+function game.Create(USER, DATA)
+	local Table = {}
+	for i, v in ipairs(DATA) do
+		Table[i] = utils[USER](unpack(v)) end
+	return Table end
+function game.GetString(STRING)
+	return DataSheet[STRING and STRING:upper()] or DataSheet["RAW"] end
+--PERFORM boolean
+--... {action, data}
+function game.Perform(PERFORM, ...)
+	local CreateEA = {}
+	for i, v in ipairs({...}) do
+		CreateEA[i] = u.CreateEA(v[1], v[2]) end
+	if not (PERFORM) then
+		return CreateEA end
+	actions.PerformBatch(CreateEA) end
 
 function kit.FindFirst(TABLE, START)
 	local Start, End = 1, #TABLE
@@ -97,8 +127,6 @@ function game.FirstLast(TABLE, START, END)
 	local START, END = game.SetupSelection(START, END)
 	local START, END = kit.FindFirst(TABLE, START), kit.FindLast(TABLE, END)
 	return START, END end
-function GetString(STRING)
-	return DataSheet[STRING and STRING:upper()] or DataSheet["RAW"] end
  --Returns values of TABLE's indexes START and END
   --Input: Index, Output: Table
 --¿STRING string
@@ -111,18 +139,18 @@ function game.Between(STRING, START, END, TABLE)
 	for i = START, END do
 		local v = TABLE[i]
 		Index = Index+1
-		Table[Index] = v
-	end
+		Table[Index] = v end
 	return Table, START, END end
 --¿STRING string
 function game.Get(STRING, TABLE)
-	local Sheet = GetString(STRING)
+	local Sheet = game.GetString(STRING)
 	local Table = {}
 	for i, v in ipairs(TABLE) do
 		Table[i] = Sheet(v) end
 	return Table end
+--¿STRING string
 function game.GetUnique(STRING, TABLE)
-	local Sheet = GetString(STRING)
+	local Sheet = game.GetString(STRING)
 	local Holder, Table, Index = {}, {}, 0
 	for i, v in ipairs(TABLE) do
 		local Data = Sheet(v)
@@ -136,56 +164,29 @@ function game.GetUnique(STRING, TABLE)
 --¿START ¿END numeric
 function game.Quarry(STRING, TABLE, START, END)
 	local START, END = game.FirstLast(TABLE, START, END)
-	local Sheet = GetString(STRING)
+	local Sheet = game.GetString(STRING)
 	local Table, Index = {}, 0
 	for i = START, END do
 		Index = Index+1
 		Table[Index] = Sheet(TABLE[i]) end
 	return Table end
-function game.Create(USER, DATA)
-	local Table = {}
-	for i, v in ipairs(DATA) do
-		Table[i] = utils[USER](unpack(v)) end
-	return Table end
---PERFORM boolean
---... {action, data}
-function game.Perform(PERFORM, ...)
-	local CreateEA = {}
-	for i, v in ipairs({...}) do
-		CreateEA[i] = u.CreateEA(v[1], v[2]) end
-	if not (PERFORM) then
-		return CreateEA end
-	actions.PerformBatch(CreateEA) end
-Infinite = 1/0
-function StartCruor(TABLE)
+
+function StartCruor(TIMES)
 	local Start, End = _Bookmarks.Start, _Bookmarks.End
-	local Times, Notes =  _Bookmarks.Times, _Bookmarks.Notes
-	_Bookmarks.Active = true
+	local Notes = _Bookmarks.Notes
+	local Table = {}
+	local Index = 0
 	for i = Start, End do
-		local Time = Times[i-Start+1]
-		if not Time then _Bookmarks.Start = i return end
-		_Bookmarks.Marks[i] = {Time.StartTime, Notes[i]}
+		Index = Index + 1
+		local Time = TIMES[Index]
+		if not Time then
+			_Bookmarks.Start = i
+			return Table, true end
+		Table[Index] = {Time.StartTime, Notes[i]}
 	end
-	_Bookmarks.Active = false
+	return Table, false
 end
-_Size =
-	{
-	Infinite = vector.new(-1, -1),
-	Regular = vector.new(0, 0),
-	Micro = vector.new(1, 1),
-	Button = vector.new(80, 20),
-	}
-_Bookmarks =
-	{
-	Active = false,
-	Notes = {},
-	Start = 0,
-	End = 0,
-	InText = "a b c",
-	Marks = {},
-	Quarry = {},
-	Remove = {},
-	}
+
 function BookmarksMaker()
 	local Size = _Size.Button
 	local Place = iButton("Place", Size)
@@ -194,47 +195,51 @@ function BookmarksMaker()
 	if #_Bookmarks.InText ~= 0 then
 		imgui.SameLine()
 		local Clear = iButton("Clear", Size)
-		if Clear then _Bookmarks.InText = "" end
-	end
+		if Clear then _Bookmarks.InText = "" end end
 	if _Bookmarks.Active then
 		imgui.SameLine()
 		local Reset = iButton("Cycle", Size)
-		if Reset then _Bookmarks.Active = false end
-	end
+		if Reset then _Bookmarks.Active = false end end
 	if Place then
-		_Bookmarks.Times = game.GetUnique("HO", state.SelectedHitObjects)
 		local RAW = game.Quarry("RAW", map.Bookmarks)
 		local BM = game.Get("BM", RAW)
+		local _BM = _Bookmarks
+		local Remove, Holder = {}, {}
 		if not _Bookmarks.Active then
-			_Bookmarks.Notes = string.split(_Bookmarks.InText)
-			_Bookmarks.Start = 1
-			_Bookmarks.End = #_Bookmarks.Notes
-			_Bookmarks.Marks = {} end
-		StartCruor(BM)
-		local Addite = game.Create("CreateBookmark", _Bookmarks.Marks)
-		local Remove = {}
-		local Holder = {}
-		for i, v in ipairs(_Bookmarks.Marks) do
-			if not Holder[v[1]] then
-				Holder[v[1]] = true
+			_BM.Notes = string.split(_BM.InText)
+			_BM.Start = 1
+			_BM.End = #_BM.Notes
+			_BM.Marks = {}
+		end
+		_BM.Marks, _BM.Active = StartCruor(game.GetUnique("HO", state.SelectedHitObjects))
+		local Addite = game.Create("CreateBookmark", _BM.Marks)
+		for i, v in ipairs(_BM.Marks) do
+			local V1 = v[1]
+			if not Holder[V1] then
+				Holder[V1] = true
 		end end
 		for i, v in ipairs(BM) do
-			if not Holder[v.StartTime] then
-				Holder[v.StartTime] = true
+			local V1 = v.StartTime
+			if not Holder[V1] then
+				Holder[V1] = true
 			else
 				Remove[#Remove+1] = RAW[i]
 		end end
 		actions.PerformBatch(
 			{
 			u.CreateEA(40, Addite),
-			u.CreateEA(43, Remove)
+			u.CreateEA(43, Remove),
 			})
 	end
 	if Remove then
 		local Bookmarks = game.Quarry("RAW", map.Bookmarks)
-		actions.Perform(u.CreateEA(43, Bookmarks)) end
+		local Count = #Bookmarks
+		if Count ~= 0 then
+			print("i!", "Removed: ".. Count.. " Bookmarks")
+			actions.Perform(u.CreateEA(43, Bookmarks))
+	end end
 	local Active, OutText = imgui.InputTextMultiline("##BookmarkInputer", _Bookmarks.InText, 9999, _Size.Infinite)
-	if Active and imgui.IsItemDeactivatedAfterEdit() then
+	if Active then
 		_Bookmarks.InText = OutText end
 end
 function fixYamlValues(TABLE)
